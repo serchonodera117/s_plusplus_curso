4bebb31ce88031e605fa65d6753280eb002dab73
serchonodera117 <sergio.lobo117@gmail.com>
2023-05-24 17:32:05 -0600

    practice of the behaviour of variables un c++

--- declaracion_iniciacion_variables/ejemplo_comportamiento_variables.cpp
+++ declaracion_iniciacion_variables/ejemplo_comportamiento_variables.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 
 int main(int argc, char** argv){
-    
+    string nombre = "Serch";
+    int edad {21};
+    float salario;
+
+
+
+    cout << "introduce el salario del empledo: "; //reead console
+    cin >> salario;
+
+    cout <<"Datos del empledo:"<<endl;
+    cout <<"Nombre:" << nombre << ". edad;"<< edad <<". Salario:" << salario; 
+
+
+  cout <<"----------------------"<< endl;
+    cout <<"Modificación de edad, introduzca nueva edad"<<endl;
+    cin >> edad;
+
+    cout <<"nueva edad:"<<" "<<edad;
+
     return 0;
 }
