ea2030363f8a0606ac3bccd6cd8d0048760afb5f
serchonodera117 <sergio.lobo117@gmail.com>
2023-06-05 10:11:04 -0600

    use of & in a parameter, explication and use

--- operators_with_conditionals/op_conditinal.cpp
+++ operators_with_conditionals/op_conditinal.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+void torLowerCase(string &cadena){ //the use of & before variable, is used to indicate that this variable is a reference
+    for(int i = 0; i <cadena.length(); i++){ //therefore, the parameter will modify the original variable that is used as a parameter when you call the function. 
+        cadena[i] = tolower(cadena[i]);
+    }
+}
+
 void conditionals(){
     int age;
 
@@ -14,7 +20,28 @@ void conditionals(){
     }
 }
 
+void logicOperators(){
+     int age;
+     string medic_exam;
+
+    cout << "How are you: ";
+    cin >> age;
+
+    cout << "Have you succesfully passed the medic exam ? (yes/no) : ";
+    cin >> medic_exam;
+
+    torLowerCase(medic_exam);
+
+    if(age >= 18 && medic_exam == "yes"){
+        cout << "You are legally Authorized to drive cars" <<endl;
+    }else{
+        cout << "You are NOT legally Authorized to drive cars, you don't meet the requirements" <<endl;
+    }
+
+}
+
 int main(){
-    conditionals();
+    logicOperators();
+    // conditionals();
     return 0;
 }
