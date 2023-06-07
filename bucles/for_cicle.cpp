9297c5ff824ce8368b8ec15a9b1ccc666ca982a7
serchonodera117 <sergio.lobo117@gmail.com>
2023-06-06 21:13:06 -0600

    use of basic for to explain the syntaxis

--- bucles/for_cicle.cpp
+++ bucles/for_cicle.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 
 int main(int argc, char **argv){//simple for
-    for(int i = 0; i < 10; i++){//for(counter: condition: counter increment or decrement)
-        cout << "iteration number:"<< i << endl;
+   int edades[] = {10,10,33,32,12,345,24};
+   int eLength =  sizeof(edades) / sizeof(edades[0]);
+        cout << eLength  << endl;
+   
+
+    for(int i = 0; i < eLength; i++){//for(counter: condition: counter increment or decrement)
+        cout << "edad de persona : "<< i << " es: "<< edades[i]  << endl;
     }
     return 0; 
 }
