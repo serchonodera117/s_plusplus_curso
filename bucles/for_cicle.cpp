da0bdabca8a5f3b875b3385311c9a4af70ddbacb
serchonodera117 <sergio.lobo117@gmail.com>
2023-06-06 21:04:32 -0600

    simple for cycle just to explain

---
+++ bucles/for_cicle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv){//simple for
+    for(int i = 0; i < 10; i++){//for(counter: condition: counter increment or decrement)
+        cout << "iteration number:"<< i << endl;
+    }
+    return 0; 
+}
