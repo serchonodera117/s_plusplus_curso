1f609aef8cee5c568a0560c721ae99df1d294ff1
serchonodera117 <sergio.lobo117@gmail.com>
2024-02-20 15:16:57 -0600

    Update: recursive functions with factorial

--- functions/recursive_functions.cpp
+++ functions/recursive_functions.cpp
@@ -2,6 +2,50 @@
 using namespace std;
 
 
+int myRecursionFactorial(int n){
+    if(n==1){
+        return 1;
+    }
+    return myRecursionFactorial(n) * myRecursionFactorial(n-1);
+}
+
+int factorialFor(int n){
+    int result = n;
+    for (int i= n-1; i >= 1; i--){
+        result*=i;
+    }
+    return result;
+}
+
+int factorialRecursionFactorial(int n){
+    
+    if(n==0){
+        return 1;
+    }else{
+        return n*factorialRecursionFactorial(n-1);
+    }
+
+}
+
+void printFactorial(){
+    int number;
+    cout << "Introduce a number to calculate factorial: ";
+    cin >> number;
+
+    cout<< "[" << number <<"] factorial is: "<< factorialFor(number) << endl;
+}
+
+void printRecursionFactorial(){
+    int number;
+    cout << "Introduce a number to calculate factorial: ";
+    cin >> number;
+
+    cout<< "[" << number <<"] factorial is: "<< factorialRecursionFactorial(number) << endl;
+}
+
 int main(int argc, char** argv){
+   //printFactorial();
+   printRecursionFactorial();
+
     return 0;
 }
